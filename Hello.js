25b8ed2d2a0abad6576ac8327e6ef913538c84d3
gudiya <gudiya.sarraf@gmail.com>
2025-01-11 14:51:20 +0530

    first commit

---
+++ Hello.js
@@ -0,0 +1,6 @@
+const accoundId=1234;
+let accoundEmail="gudiya@gmail.com"
+var accountCity="Bangalore";
+accountState="Krnataka"
+console.table([accoundId, accoundEmail, accountCity,accountState]);
+console.log("Hii Gudiya");
